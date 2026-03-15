d8a8da05cecea7f90222e38ecb9d8804fe29cab9
Kacper Tarchała <tarchala.k@gmail.com>
2026-03-15 22:35:08 +0100

    feat: auto-track rate changes on project membership

--- app/models/project_membership.rb
+++ app/models/project_membership.rb
@@ -4,4 +4,27 @@ class ProjectMembership < ApplicationRecord
   has_many :rate_changes, dependent: :destroy
 
   validates :user_id, uniqueness: { scope: :project_id }
+
+  after_create :record_initial_rate
+  after_update :record_rate_change, if: :saved_change_to_hourly_rate_cents?
+
+  private
+
+  def record_initial_rate
+    rate_changes.create!(
+      hourly_rate_cents: hourly_rate_cents,
+      previous_rate_cents: nil,
+      changed_by: Current.user,
+      changed_at: Time.current
+    )
+  end
+
+  def record_rate_change
+    rate_changes.create!(
+      hourly_rate_cents: hourly_rate_cents,
+      previous_rate_cents: hourly_rate_cents_before_last_save,
+      changed_by: Current.user,
+      changed_at: Time.current
+    )
+  end
 end
--- test/models/project_membership_test.rb
+++ test/models/project_membership_test.rb
@@ -33,4 +33,33 @@ class ProjectMembershipTest < ActiveSupport::TestCase
 
     assert_not ProjectMembership.exists?(user: user, project: projects(:jira_project))
   end
+
+  test "creates rate_change on create" do
+    pm = ProjectMembership.create!(
+      project: projects(:plain_project),
+      user: users(:two),
+      hourly_rate_cents: 5000
+    )
+    assert_equal 1, pm.rate_changes.count
+    rc = pm.rate_changes.first
+    assert_equal 5000, rc.hourly_rate_cents
+    assert_nil rc.previous_rate_cents
+  end
+
+  test "creates rate_change on rate update" do
+    pm = project_memberships(:one_elvium)
+    assert_difference "RateChange.count", 1 do
+      pm.update!(hourly_rate_cents: 20000)
+    end
+    rc = pm.rate_changes.order(:created_at).last
+    assert_equal 20000, rc.hourly_rate_cents
+    assert_equal 15000, rc.previous_rate_cents
+  end
+
+  test "does not create rate_change when rate unchanged" do
+    pm = project_memberships(:one_elvium)
+    assert_no_difference "RateChange.count" do
+      pm.update!(updated_at: Time.current)
+    end
+  end
 end
