cbca3b0dade520827e886fcba1b091fbfc7b1655
Kacper Tarchała <tarchala.k@gmail.com>
2026-06-02 19:56:48 +0200

    feat: gate Clients nav link on feature flag; add Workspace Settings menu link

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -17,4 +17,30 @@ class ClientsControllerTest < ActionDispatch::IntegrationTest
     get clients_path
     assert_response :success
   end
+
+  test "sidebar shows the Clients link when the feature is enabled for an admin" do
+    @workspace.update!(clients_enabled: true)
+    sign_in_as(users(:one))
+    get root_path
+    assert_select "a[href=?]", clients_path
+  end
+
+  test "sidebar hides the Clients link when the feature is disabled" do
+    @workspace.update!(clients_enabled: false)
+    sign_in_as(users(:one))
+    get root_path
+    assert_select "a[href=?]", clients_path, count: 0
+  end
+
+  test "sidebar shows the Workspace Settings link for an admin" do
+    sign_in_as(users(:one))
+    get root_path
+    assert_select "a[href=?]", workspace_settings_path
+  end
+
+  test "sidebar hides the Workspace Settings link for an employee" do
+    sign_in_as(users(:two))
+    get root_path
+    assert_select "a[href=?]", workspace_settings_path, count: 0
+  end
 end
